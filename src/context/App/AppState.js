304d31b14805041787a2759796ae9613dc4d514b
xristmas365 <50219934+xristmas365@users.noreply.github.com>
2020-11-03 15:01:58 +0300

    Static Nutrition URL

--- src/context/App/AppState.js
+++ src/context/App/AppState.js
@@ -42,8 +42,7 @@ export const AppState = ({children}) => {
       AsyncStorage.getItem('login'),
       AsyncStorage.getItem('password')
     ]);
-    let url = 'mynutritionapp://';
-    url += '3090902.ra@gmail.com' + ':' + '123456';
+    let url = 'mynutritionapp://3090902.ra@gmail.com:12345';
     if (await Linking.canOpenURL(url)) {
       try {
         await Linking.openURL(url)
